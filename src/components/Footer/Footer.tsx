6ec360853f04aa8568b16c3d654d280c795f9af8
DynamoAbir <mhabir10@gmail.com>
2025-02-13 16:40:31 +0600

    before shadcn

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,29 +3,28 @@ const Footer = () => {
     <div className="grid grid-cols-2 md:grid-cols-4 w-full px-28 py-10 mt-24 bg-[#F5F5F5] gap-10">
       <div>
         <h1 className="font-semibold mb-5">Contact</h1>
-        <p>Contact Us</p>
-        <p>Website:https://maya.io</p>
+        <p className="text-sm">Contact Us</p>
+        <p className="text-sm">Website:https://maya.io</p>
       </div>
       <div>
         <h1 className="font-semibold mb-5">About</h1>
-        <p>Support Center</p>
-        <p>Customer Support</p>
-        <p>About Us</p>
-        <p>Copyright</p>
+        <p className="text-sm">Support Center</p>
+        <p className="text-sm">Customer Support</p>
+        <p className="text-sm">About Us</p>
+        <p className="text-sm">Copyright</p>
       </div>
       <div>
-        <h1 className="font-semibold mb-5">About</h1>
-        <p>Privacy policy update</p>
-        <p>Terms & conditions</p>
-        <p>Return Policy</p>
-        <p>Site Map</p>
+        <h1 className="font-semibold mb-5">Customer Care</h1>
+        <p className="text-sm">FAQ & Helps</p>
+        <p className="text-sm">Shipping & Delivery</p>
+        <p className="text-sm">Return & Exchanges</p>
       </div>
       <div>
-        <h1 className="font-semibold mb-5">About</h1>
-        <p>Privacy policy update</p>
-        <p>Terms & conditions</p>
-        <p>Return Policy</p>
-        <p>Site Map</p>
+        <h1 className="font-semibold mb-5">Our Information</h1>
+        <p className="text-sm">Privacy policy update</p>
+        <p className="text-sm">Terms & conditions</p>
+        <p className="text-sm">Return Policy</p>
+        <p className="text-sm">Site Map</p>
       </div>
     </div>
   );
